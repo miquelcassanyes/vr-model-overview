09c97f5cd88e39015b3030f62da684244b208175
Miquel Cassanyes <miquel.cassanyes@netcentric.biz>
2018-10-30 11:15:33 +0100

    Basic setup created

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/dist'
+    },
+    mode: 'none'
+};
