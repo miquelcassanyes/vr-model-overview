986ea601e08a09c4a410a67357071ef639e5ef48
Miquel Cassanyes <miquel.cassanyes@netcentric.biz>
2018-11-08 19:59:10 +0100

    Removed unnecessary file and console.logs

--- src/js/index.js
+++ src/js/index.js
@@ -2,9 +2,6 @@ import 'aframe';
 import 'aframe-extras';
 import 'aframe-teleport-controls';
 
-import { test } from './components/test';
 import { gazeTeleport } from './components/gaze-teleport';
 import { mouseTeleport } from './components/mouse-teleport';
 import { universalTeleport } from './components/universal-teleport';
-
-console.log('index js');
