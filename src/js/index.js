13aaa242256d10b564dc2ab1f728a1c6a98fc523
Miquel Cassanyes <miquel.cassanyes@netcentric.biz>
2018-11-15 11:13:38 +0100

    Cleanup models

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import 'aframe';
 import 'aframe-extras';
 import 'aframe-teleport-controls';
 import 'aframe-environment-component';
+import 'aframe-stats-in-vr-component';
 
 import { extendedTeleport} from './components/extended-teleport';
